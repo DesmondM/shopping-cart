c85d0fde8f599653bb0c3760bef81a9f141263e4
DesmondM <desmondmpofu@gmail.com>
2021-10-17 14:56:23 +0200

    headers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
+import Header from './components/header/Header';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Test
+      <Header/>
       </header>
     </div>
   );
