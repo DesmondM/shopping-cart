b41e4d8fb8f8e280bff524d7384833e197e4cf19
DesmondM <desmondmpofu@gmail.com>
2021-10-19 23:50:51 +0200

    additions

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import ProductsContext from './context/ProductsContext';
 
 ReactDOM.render(
   <BrowserRouter>
+  <ProductsContext>
     <App />
+    </ProductsContext>
   </BrowserRouter>,
   document.getElementById('root')
 );
