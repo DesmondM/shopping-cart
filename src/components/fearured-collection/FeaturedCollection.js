9c117b8edd691a1ecea0f8cefe13ea364b947fe5
DesmondM <desmondmpofu@gmail.com>
2021-10-21 00:08:37 +0200

    featured

--- src/components/fearured-collection/FeaturedCollection.js
+++ src/components/fearured-collection/FeaturedCollection.js
@@ -1,8 +1,14 @@
 import React, {useContext} from 'react';
-import {ProductsContext} from 'from'
+import {ProductsCont, ProductsContext} from '../../context/ProductsContext';
+import FeaturedProduct from '../shared/FeaturedProduct';
+
 
 
 const FeaturedCollection = () => {
+    const {products} =useContext(ProductsCont);
+    const productItems= products.filter((product, i)=>i<4).map(product=>(
+        <FeaturedProduct {...product} key={product.id}/>
+    ))
     return (
         <div>
             
